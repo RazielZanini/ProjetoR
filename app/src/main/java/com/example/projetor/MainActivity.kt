c25a34887055817bcca94420846dcae938e14b12
Raziel <raziel@za9.com.br>
2025-02-12 17:31:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projetor/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.projetor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+    }
+}
